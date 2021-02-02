f03e15ee98fac357b2d0fd6adafc987f751be168
Артем Лазарчик <lazarchik_2000@mail.ru>
2021-02-02 12:04:28 +0200

    11

--- Auto/Program.cs
+++ Auto/Program.cs
@@ -10,12 +10,19 @@ namespace Auto
     {
         static void Main(string[] args)
         {
-            Console.Write($"Введите калорийность в 100г продукта: ");
-            int calorie = int.Parse(Console.ReadLine());
-            Console.Write($"Введите массу продукта в кг: ");
-            double weight = double.Parse(Console.ReadLine());
-            Power power = new Power(calorie,weight);
-            Console.WriteLine($"{power.Product()} калорий");
+            try
+            {
+                Console.Write($"Введите калорийность в 100г продукта: ");
+                int calorie = int.Parse(Console.ReadLine());
+                Console.Write($"Введите массу продукта в кг: ");
+                double weight = double.Parse(Console.ReadLine());
+                Power power = new Power(calorie, weight);
+                Console.WriteLine($"{power.Product()} калорий");
+            }
+            catch(Exception er)
+            {
+                Console.WriteLine(er.Message);
+            }
             Console.ReadLine();
         }
     }
