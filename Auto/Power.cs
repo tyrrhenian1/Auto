95a9436827510ebf444422c9faab0605950a9ba1
Артем Лазарчик <lazarchik_2000@mail.ru>
2021-02-02 04:00:46 +0200

    Добавьте файлы проекта.

---
+++ Auto/Power.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    class Power
+    {
+        public int Calorie { get; set; }
+        public double Weight { get; set; }
+        public Power(int calorie, double weight)
+        {
+            Calorie = calorie;
+            Weight = weight;
+        }
+        public double Product()
+        {
+            double p = Weight * 10 * Calorie;
+            return p;
+        }
+    }
+}
---
+++ Auto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Введите калорийность в 100г продукта: ");
+            int calorie = int.Parse(Console.ReadLine());
+            Console.Write($"Введите массу продукта в кг: ");
+            double weight = double.Parse(Console.ReadLine());
+            Power power = new Power(calorie,weight);
+            Console.WriteLine($"{power.Product()} калорий");
+            Console.ReadLine();
+        }
+    }
+}
